5a26a91c88f4a5a8f1f8032f37758972bc979941
Gustavo Caçador <gustavoirl456@outlook.com>
2025-02-04 15:01:49 -0300

    projeto criado

---
+++ src/test/java/br/com/gustavo/sistema_evento/SistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.sistema_evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
